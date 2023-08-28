033e9387c9ff9cf10f5c0e7e0386d66d15fd1d37
Shu Wang <shugewang@hotmail.co.uk>
2023-08-28 21:27:43 +0100

    Add ability to save and remove favourites from top picks

--- src/components/favourites/Favourites.tsx
+++ src/components/favourites/Favourites.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { useContext } from 'react';
-import FavouriteContext from 'src/context/FavouritesContext';
-import { XCircleIcon } from '@heroicons/react/24/outline';
-import SmallCard from '../cards/SmallCard';
+import { useContext } from "react";
+import FavouriteContext from "src/context/FavouritesContext";
+import { XCircleIcon } from "@heroicons/react/24/outline";
+import SmallCard from "../cards/SmallCard";
 
 interface FavouriteProps {
   setIsShowFavourites: (val: boolean) => void;
@@ -14,17 +14,26 @@ const Favourites = ({ setIsShowFavourites }: FavouriteProps) => {
   return (
     <div
       className="fixed inset-0 bg-[rgba(0,0,0,0.7)] ease-in-out duration-500"
-      onClick={() => setIsShowFavourites(false)}>
+      onClick={() => setIsShowFavourites(false)}
+    >
       <div
         className="bg-white w-[350px] h-full absolute right-0 ease-in-out duration-300"
-        onClick={(e) => e.stopPropagation()}>
+        onClick={(e) => e.stopPropagation()}
+      >
         <div className="flex flex-col">
           <div className="flex flex-row justify-between relative top-28 mx-4 items-center bg-orange-600 p-4 text-white">
-            <XCircleIcon className="h-6 cursor-pointer hover:scale-105" onClick={() => setIsShowFavourites(false)} />
+            <XCircleIcon
+              className="h-6 cursor-pointer hover:scale-105"
+              onClick={() => setIsShowFavourites(false)}
+            />
             <h1 className="text-md">Favourites&nbsp;({totalFavourites})</h1>
           </div>
 
-          {favourites.length === 0 && <span className="relative text-sm mt-36 m-6">You have no favourites saved</span>}
+          {favourites.length === 0 && (
+            <span className="relative text-sm mt-36 m-6">
+              You have no favourites saved
+            </span>
+          )}
 
           <ul className="relative mt-36 mx-4">
             {favourites?.map((favourite) => (
--- src/components/topPicks/TopPicks.tsx
+++ src/components/topPicks/TopPicks.tsx
@@ -1,28 +1,21 @@
-import { useEffect, useState } from 'react'
-import Axios from 'src/api/Axios';
-import Skeleton from '../skeleton/Skeleton';
-import { Link } from 'react-router-dom';
-import PropertyType from 'src/interfaces/Property';
-import MediumCard from '../cards/MediumCard';
-import { useFetchTopPicks } from 'src/hooks/useFetchTopPicks';
+import Skeleton from "../skeleton/Skeleton";
+import PropertyType from "src/interfaces/Property";
+import MediumCard from "../cards/MediumCard";
+import { useFetchTopPicks } from "src/hooks/useFetchTopPicks";
 
 const TopPicks = () => {
   const { isLoading, data } = useFetchTopPicks();
 
-  if (isLoading) return <Skeleton />
+  if (isLoading) return <Skeleton />;
 
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xlg:grid-cols-4'>
-      {!isLoading && 
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xlg:grid-cols-4">
+      {!isLoading &&
         data?.data.map((topPickProperty: PropertyType, tpId: number) => (
-          <Link to={`/property/${topPickProperty._id}`} key={`top-pick-${tpId}`}>
-            <MediumCard property={topPickProperty} />
-          </Link>
-        ))
-
-      }
+          <MediumCard key={`top-pick-${tpId}`} property={topPickProperty} />
+        ))}
     </div>
-  )
-}
+  );
+};
 
-export default TopPicks
+export default TopPicks;
