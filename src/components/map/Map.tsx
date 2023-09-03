e6c0767515c80ec9d4cafa94fa8060a3cb244fac
Shu Wang <shugewang@hotmail.co.uk>
2023-09-03 23:23:26 +0100

    Clicking on map marker navigates to property page

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -5,6 +5,7 @@ import {
   getCoordinatesForRegion,
 } from 'src/util/MapCoordinates';
 import PropertyType from 'src/interfaces/Property';
+import { useNavigate } from 'react-router-dom';
 
 interface MapProps {
   searchLocation: string;
@@ -16,6 +17,8 @@ export const Map = ({ searchLocation, region, searchResults }: MapProps) => {
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY!,
   });
+
+  const navigate = useNavigate();
 
   const [center, setCenter] = useState({ lat: 0, lon: 0 });
 
@@ -40,7 +43,7 @@ export const Map = ({ searchLocation, region, searchResults }: MapProps) => {
       mapContainerClassName="absolute top-0 left-0 w-full h-full"
     >
       {searchResults?.map((result, index) => (
-        <Marker key={index} position={{ lat: result.lat, lng: result.lon }} />
+        <Marker key={index} position={{ lat: result.lat, lng: result.lon }} onClick={() => navigate(`/property/${result._id}`)} />
       ))}
     </GoogleMap>
   );
